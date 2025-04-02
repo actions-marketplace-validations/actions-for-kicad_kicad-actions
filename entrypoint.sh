59cc821334d1a7e0242586f88f8844311f8abe47
Nick van Leeuwen <nickvanleeuwen01@gmail.com>
2025-04-02 21:11:57 +0200

    Added first PCB outputs and tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ drc_violation=0 # DRC exit code
 
 
 # Check if any schematic output/erc are selected without the file being present
-if [[ -z $INPUT_SCHEMATIC_FILE && (
+if [[ -z $INPUT_SCHEMATIC_FILE_NAME && (
     $INPUT_RUN_ERC == "true" || 
     $INPUT_SCHEMATIC_OUTPUT_PDF == "true" || 
     $INPUT_SCHEMATIC_OUTPUT_SVG == "true" || 
@@ -21,79 +21,86 @@ if [[ -z $INPUT_SCHEMATIC_FILE && (
     $INPUT_SCHEMATIC_OUTPUT_NETLIST == "true" )
 ]]; then
     echo "::error::Schematic output/ERC options selected without a schematic file."
+    exit 1
 fi
 
 # Check if any PCB output/drc are selected without the file being present
-if [[ -z $INPUT_PCB_FILE && (
-    $INPUT_RUN_DRC == "true" )
+if [[ -z $INPUT_PCB_FILE_NAME && (
+    $INPUT_PCB_OUTPUT_DRILL == "true" )
 ]]; then
     echo "::error::PCB output/DRC options selected without a PCB file."
+    exit 1
 fi
 
 # Run schematic outputs
-if [[ -n $INPUT_SCHEMATIC_FILE ]]; then
+if [[ -n $INPUT_SCHEMATIC_FILE_NAME ]]; then
   if [[ $INPUT_RUN_ERC == "true" ]]; then
     kicad-cli sch erc \
       --output "$INPUT_ERC_OUTPUT_FILE_NAME" \
       --exit-code-violations \
-      "$INPUT_SCHEMATIC_FILE"
+      "$INPUT_SCHEMATIC_FILE_NAME"
     erc_violation=$?
   fi
 
   if [[ $INPUT_SCHEMATIC_OUTPUT_PDF == "true" ]]; then
     cmd=(kicad-cli sch export pdf --output "$INPUT_SCHEMATIC_OUTPUT_PDF_FILE_NAME")
     [[ $INPUT_SCHEMATIC_OUTPUT_BLACK_WHITE == "true" ]] && cmd+=(--black-and-white)
-    "${cmd[@]}" "$INPUT_SCHEMATIC_FILE"
+    "${cmd[@]}" "$INPUT_SCHEMATIC_FILE_NAME"
   fi
 
   if [[ $INPUT_SCHEMATIC_OUTPUT_SVG == "true" ]]; then
     cmd=(kicad-cli sch export svg --output "$INPUT_SCHEMATIC_OUTPUT_SVG_FOLDER_NAME")
     [[ $INPUT_SCHEMATIC_OUTPUT_BLACK_WHITE == "true" ]] && cmd+=(--black-and-white)
-    "${cmd[@]}" "$INPUT_SCHEMATIC_FILE"
+    "${cmd[@]}" "$INPUT_SCHEMATIC_FILE_NAME"
   fi
 
   if [[ $INPUT_SCHEMATIC_OUTPUT_DXF == "true" ]]; then
     cmd=(kicad-cli sch export dxf --output "$INPUT_SCHEMATIC_OUTPUT_DXF_FOLDER_NAME")
     [[ $INPUT_SCHEMATIC_OUTPUT_BLACK_WHITE == "true" ]] && cmd+=(--black-and-white)
-    "${cmd[@]}" "$INPUT_SCHEMATIC_FILE"
+    "${cmd[@]}" "$INPUT_SCHEMATIC_FILE_NAME"
   fi
 
   if [[ $INPUT_SCHEMATIC_OUTPUT_HPGL == "true" ]]; then
     cmd=(kicad-cli sch export hpgl --output "$INPUT_SCHEMATIC_OUTPUT_HPGL_FOLDER_NAME")
     [[ $INPUT_SCHEMATIC_OUTPUT_BLACK_WHITE == "true" ]] && cmd+=(--black-and-white)
-    "${cmd[@]}" "$INPUT_SCHEMATIC_FILE"
+    "${cmd[@]}" "$INPUT_SCHEMATIC_FILE_NAME"
   fi
 
   if [[ $INPUT_SCHEMATIC_OUTPUT_PS == "true" ]]; then
     cmd=(kicad-cli sch export ps --output "$INPUT_SCHEMATIC_OUTPUT_PS_FOLDER_NAME")
     [[ $INPUT_SCHEMATIC_OUTPUT_BLACK_WHITE == "true" ]] && cmd+=(--black-and-white)
-    "${cmd[@]}" "$INPUT_SCHEMATIC_FILE"
+    "${cmd[@]}" "$INPUT_SCHEMATIC_FILE_NAME"
   fi
 
   if [[ $INPUT_SCHEMATIC_OUTPUT_BOM == "true" ]]; then
     kicad-cli sch export bom \
       --output "$INPUT_SCHEMATIC_OUTPUT_BOM_FILE_NAME" \
-      "$INPUT_SCHEMATIC_FILE"
+      "$INPUT_SCHEMATIC_FILE_NAME"
   fi
 
   if [[ $INPUT_SCHEMATIC_OUTPUT_NETLIST == "true" ]]; then
     kicad-cli sch export netlist \
       --output "$INPUT_SCHEMATIC_OUTPUT_NETLIST_FILE_NAME" \
-      "$INPUT_SCHEMATIC_FILE"
+      "$INPUT_SCHEMATIC_FILE_NAME"
   fi
 fi
 
 # Run DRC
-if [[ -n $INPUT_SCHEMATIC_FILE ]]; then
+if [[ -n $INPUT_PCB_FILE_NAME ]]; then
   if [[ $INPUT_RUN_DRC == "true" ]]; then
     kicad-cli pcb drc \
       --output "$INPUT_DRC_OUTPUT_FILE_NAME" \
       --exit-code-violations \
-      "$INPUT_PCB_FILE"
+      "$INPUT_PCB_FILE_NAME"
     drc_violation=$?
   fi
 
-
+  if [[ $INPUT_PCB_OUTPUT_DRILL == "true" ]]; then
+    kicad-cli pcb export drill \
+      --output "$INPUT_PCB_OUTPUT_DRILL_FILE_NAME" \
+      --format "$INPUT_PCB_OUTPUT_DRILL_FORMAT" \
+      "$INPUT_PCB_FILE_NAME"
+  fi
 fi
 
 # Return non-zero exit code for ERC or DRC violations
